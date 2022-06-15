67e594d12515e169da27690b64b94de6fc011690
wandaprasetya23 <107533381+wandaprasetya23@users.noreply.github.com>
2022-06-15 12:56:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['628975217200'],
+  ['628975217200'],
+  ['628975217200', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '𝐰𝐚𝐧𝐝𝐚 𝐠𝐚𝐧𝐬'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
